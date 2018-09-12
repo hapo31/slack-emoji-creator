5dffcbe74b3d0bc78f8959ff5701d244c314dca8
happou31 <happo31@gmail.com>
2018-09-12 19:25:44 +0900

    wip: Handler書く

--- server.py
+++ server.py
@@ -1,17 +1,22 @@
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import SocketServer
 
 
-def run(server=HTTPServer, port=5000):
+class HttpHandler(BaseHTTPRequestHandler):
+    def __init__(self):
+        super.__init__()
+
+
+def run(token, server=HTTPServer, port=5000):
     address = ("", port)
     httpd = server(address, BaseHTTPRequestHandler)
     httpd.serve_forever()
 
 
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv) > 2:
-        port = int(sys.argv[1])
-        run(port=port)
-    else:
-        run()
+
+    import os
+
+    port = int(os.environ["SERVE_PORT"])
+    token = os.environ["HOOK_URL"]
+    run(token=token, port=port)
