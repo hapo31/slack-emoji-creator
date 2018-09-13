462c0b2e34bd2efc975f190f9ae64bacc18bbdaf
happou31 <happo31@gmail.com>
2018-09-13 12:56:33 +0900

    add: チャレンジレスポンス返せるように

--- server.py
+++ server.py
@@ -1,23 +1,38 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
+import json
 
 
 class HttpHandler(BaseHTTPRequestHandler):
-    def __init__(self):
-        super.__init__(self)
-        self._slack_hook_url = os.environ["SLACK_HOOK_URL"]
-
     def do_POST(self):
-        self.send_response(200)
-        text = "Work!"
-        self.send_header('Content-Length', len(text))
-        self.wfile.write(text.encode("utf-8"))
+
+        content_len = int(self.headers.get('content-length'))
+        requestBody = json.loads(self.rfile.read(content_len).decode('utf-8'))
+
+        res = ""
+        if requestBody["type"] == "url_verification":
+            # チャレンジレスポンスを返す
+            res = json.dumps(requestBody)
+
+        elif requestBody["type"] == "event_callback":
+            pass  # TODO
+
+        self._set_content_length(res)
+        self.end_headers()
+
+        self.wfile.write(res.encode("utf-8"))
 
     def do_GET(self):
-        self.send_response(200)
         text = "Work!"
+
+        self.send_response(200)
         self.send_header('Content-Length', len(text))
+        self.end_headers()
+
         self.wfile.write(text.encode("utf-8"))
+
+    def _set_content_length(self, text: str):
+        self.send_header("Content-Length", len(text))
 
 
 def run(server=HTTPServer, port=5000):
