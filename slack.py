399c280709cd560904c9befc4d0586f0846b100d
happou31 <happo31@gmail.com>
2018-09-13 21:34:21 +0900

    add: 絵文字関連

--- slack.py
+++ slack.py
@@ -1,21 +1,46 @@
 import requests
 from emojigen import fetch_emoji, create_emoji_url
+from post_emoji import post_emoji
 
 from post_tool import post_json
 
+SHARE_PUBLIC_URL = "/api/files.sharedPublicURL"
+
 
 class Slack():
-    def __init__(self, hook_url: str):
-        self._url = hook_url
-
-    def post_image_url(self, text: str, back_color="FFFFFF00", color="EC71A1FF", font="notosans-mono-bold", size_fixed=False, stretch=True):
-        emojigen_url = create_emoji_url(
-            text, back_color, color, font, False, size_fixed, stretch)
-        self.post_message(emojigen_url)
+    def __init__(self, base_url: str, hook_url: str, oauth_token: str):
+        self._base_url = base_url
+        self._hook_url = hook_url
+        self._oauth_token = oauth_token
 
     def post_message(self, text):
         param = {
             "text": text
         }
-        res = post_json(self._url, data=param)
+        res = post_json(self._hook_url, data=param)
         res.raise_for_status()
+        return res
+
+    def fetch_events_files(self, events_files: list):
+        files = []
+        if len(events_files) > 0:
+            for file in events_files:
+                if "id" in file and "permalink_public" in file and "mimetype" in file:
+                    # 画像以外のファイルは飛ばす
+                    if file["mimetype"].find("image") == -1:
+                        continue
+
+                    # まずファイルをpublic状態にする
+                    res = post_json("{}{}".format(self._base_url, SHARE_PUBLIC_URL), {
+                                    "token": self._oauth_token, "file": file["id"]})
+                    res.raise_for_status()
+                    # シェアされたファイルを取得する
+                    res = requests.get(file["permalink_public"])
+                    res.raise_for_status()
+                    files.append(res.content)
+        return files
+
+    def add_emoji(self, workspace: str, session, name: str, file: bytes):
+        res = post_emoji(workspace, session, name, file)
+        res.raise_for_status()
+        return res
