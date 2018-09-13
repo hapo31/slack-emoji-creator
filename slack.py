106ca2cd9433b9d9ccff7f5c2cdf576ee378235b
happou31 <happo31@gmail.com>
2018-09-13 11:57:34 +0900

    add: Slackになんか投稿するメソッド

--- slack.py
+++ slack.py
@@ -1,10 +1,19 @@
-import os
-from slackclient import SlackClient
+import requests
+from emojigen import fetch_emoji, create_emoji_url
 
 
 class SlackBot():
-    def __init__(self, token):
-        self._client = SlackClient(token)
+    def __init__(self, hook_url: str):
+        self._url = hook_url
 
-    def post_image(self):
-        self._client
+    def post_image_url(self, text: str, back_color="FFFFFF00", color="EC71A1FF", font="notosans-mono-bold", size_fixed=False, stretch=True):
+        emojigen_url = create_emoji_url(
+            text, back_color, color, font, False, size_fixed, stretch)
+        self.post_message(emojigen_url)
+
+    def post_message(self, text):
+        param = {
+            "text": text
+        }
+        res = requests.post(self._url, data=param)
+        res.raise_for_status()
