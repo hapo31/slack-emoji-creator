535bb06b2975b4c592e3a46b27eb218a5c3f77e9
happou31 <happo31@gmail.com>
2018-09-13 11:57:13 +0900

    wip: Slackのチャレンジレスポンスを返す

--- server.py
+++ server.py
@@ -1,22 +1,32 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import SocketServer
+import os
 
 
 class HttpHandler(BaseHTTPRequestHandler):
     def __init__(self):
-        super.__init__()
+        super.__init__(self)
+        self._slack_hook_url = os.environ["SLACK_HOOK_URL"]
+
+    def do_POST(self):
+        self.send_response(200)
+        text = "Work!"
+        self.send_header('Content-Length', len(text))
+        self.wfile.write(text.encode("utf-8"))
+
+    def do_GET(self):
+        self.send_response(200)
+        text = "Work!"
+        self.send_header('Content-Length', len(text))
+        self.wfile.write(text.encode("utf-8"))
 
 
-def run(token, server=HTTPServer, port=5000):
+def run(server=HTTPServer, port=5000):
     address = ("", port)
-    httpd = server(address, BaseHTTPRequestHandler)
+    httpd = server(address, HttpHandler)
     httpd.serve_forever()
 
 
 if __name__ == '__main__':
-
-    import os
-
     port = int(os.environ["SERVE_PORT"])
-    token = os.environ["HOOK_URL"]
-    run(token=token, port=port)
+    run(port=port)
